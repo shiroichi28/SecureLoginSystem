6380b21548574889633982b107b39f0fae2bd860
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-06 22:23:31 +0530

    Optmized

--- controllerModel/registration.php
+++ controllerModel/registration.php
@@ -1,64 +1,65 @@
 <?php
-require_once('../configuration/db_conf.php');
-require_once('../configuration/function.php');
+require('../configuration/db_conf.php');
+require('../configuration/function.php');
 
 session_start();
 
-$error = array();
+$errors = [];
 
-try {
-    if (isset($_POST['submit'])) {
-        $username = sanitizeInput($_POST['username']);
-        $email = sanitizeInput($_POST['email']);
-        $mobile = sanitizeInput($_POST['mobile']);
-        $password = sanitizeInput($_POST['password']);
-        $confirmPassword = sanitizeInput($_POST['confirmPassword']);
+if (isset($_POST['submit'])) {
+    $username = sanitizeInput($_POST['username']);
+    $email = sanitizeInput($_POST['email']);
+    $mobile = sanitizeInput($_POST['mobile']);
+    $password = sanitizeInput($_POST['password']);
+    $confirmPassword = sanitizeInput($_POST['confirmPassword']);
 
-        if (!validateInput($confirmPassword, 'password')) {
-            $error['cnf'] = 'Invalid Confirm Password';
-        } elseif ($password != $confirmPassword) {
-            $error['cnf'] = 'Passwords do not match';
-        }
+    if (!validateInput($confirmPassword, 'password')) {
+        $errors['cnf'] = 'Invalid Confirm Password';
+    } elseif ($password != $confirmPassword) {
+        $errors['cnf'] = 'Passwords do not match';
+    }
 
-        if (!validateInput($mobile, 'mobile')) {
-            $error['mobile'] = 'Invalid Mobile Number';
-        }
+    if (!validateInput($mobile, 'mobile')) {
+        $errors['mobile'] = 'Invalid Mobile Number';
+    }
 
-        if (!validateInput($email, 'email')) {
-            $error['email'] = 'Invalid Email';
-        }
+    if (!validateInput($email, 'email')) {
+        $errors['email'] = 'Invalid Email';
+    }
 
-        if (!validateInput($username, 'username')) {
-            $error['username'] = 'Invalid username';
-        }
+    if (!validateInput($username, 'username')) {
+        $errors['username'] = 'Invalid username';
+    }
 
-        if (!validateInput($password, 'password')) {
-            $error['password'] = 'Invalid Password';
-        }
+    if (!validateInput($password, 'password')) {
+        $errors['password'] = 'Invalid Password';
+    }
 
-        if (empty($error)) {
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-            
+    if (empty($errors)) {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        try {
             // Prepare and execute the INSERT query
             $stmt = $pdo->prepare('INSERT INTO users (username, email, mobile, password, created_on) 
-    VALUES (:username, :email, :mobile, :password, :created_on)');
+                VALUES (:username, :email, :mobile, :password, :created_on)');
             $stmt->execute([
-                'username' => $username,
+                'username' => strtoupper($username),
                 'email' => $email,
                 'mobile' => $mobile,
                 'password' => $hashedPassword,
                 'created_on' => strtotime(date('Y-m-d H:i:s'))
             ]);
-            header('Location: ../views/login.html');
-            exit(); // Add exit() after header to ensure proper redirection
-        } else {
-            $error['registrationError'] = 'Registration Unsuccessful, Check Inputs';
-            $_SESSION['error']=$error;
+
+            header('Location: ../views/login');
+            exit();
+        } catch (PDOException $e) {
+            $_SESSION['error']=$errors;
         }
+    } else {
+        $_SESSION['error']=$errors;
+
     }
-} catch (PDOException $e) {
-    $error['registrationError'] = 'Registration Unsuccessful, Check Inputs';
-    $_SESSION['error']=$error;
-    header('Location: ../views/registration.html');
-    exit();
 }
+
+header('Location: ../views/registration');
+exit();

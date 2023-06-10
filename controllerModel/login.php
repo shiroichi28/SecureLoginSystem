7881f5bc2b3346628532bd320f94040fa8d2a00e
shiroichi28 <manikandanmk2972@gmail.com>
2023-06-10 22:24:42 +0530

    Implemented CSRF

--- controllerModel/login.php
+++ controllerModel/login.php
@@ -3,15 +3,16 @@ require('../configuration/db_conf.php');
 require('../configuration/function.php');
 if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
     header('HTTP/1.0 403 Forbidden', TRUE, 403);
-    die ("<h2>Access Denied!</h2> This file is protected and not available to public.");
+    die("<h2>Access Denied!</h2> This file is protected and not available to public.");
 }
 session_start();
-
 $errors = [];
 if (isset($_POST['submit'])) {
     $emailMobile = sanitizeInput($_POST['emailMobile']);
     $password = sanitizeInput($_POST['password']);
-
+    if (!validateCSRFToken($_POST['csrf_token'])) {
+        $errors['cnf'] = 'Login Error ,Try again';
+    }
     if (empty($emailMobile) || empty($password)) {
         $errors['cnf'] = 'All fields required';
     }
@@ -31,8 +32,8 @@ if (isset($_POST['submit'])) {
             session_regenerate_id();
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_name'] = $user['username'];
-            $_SESSION["login_time_stamp"] = time(); 
-            header('Location: ../views/home');
+            $_SESSION["login_time_stamp"] = time();
+            header('Location:../views/home.php');
             exit();
         } else {
             $errors['login'] = 'Wrong Credentials';
@@ -42,5 +43,5 @@ if (isset($_POST['submit'])) {
     }
 }
 $_SESSION['error'] = $errors;
-header('Location: ../views/login');
+header('Location:../views/login.php');
 exit();
